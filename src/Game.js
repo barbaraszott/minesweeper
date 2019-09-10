c31714ed16c9eebd05a7f2c8a81a1b1cb7b82d2d
Barbara Szott <barbara.aleksy@gmail.com>
2019-09-10 18:23:03 +0200

    Initial setup

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Game.scss';
+
+class Game extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hi!</h1>
+			</div>
+		);
+	}
+}
+
+export default Game;

bc2e4f94c34057cee20e253e8c5cb00540dfa3aa
Barbara Szott <barbara.aleksy@gmail.com>
2019-09-10 18:49:27 +0200

    Create initial state in Game.js

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './Game.scss';
 
 class Game extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			height : 10,
+			width  : 10,
+			mines  : 10
+		};
+	}
+
 	render() {
 		return (
 			<div>

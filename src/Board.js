de26fbd091091ac8659eabe93a703d3f557e8197
Barbara Szott <barbara.aleksy@gmail.com>
2019-09-10 18:55:33 +0200

    Create Cell component

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Cell from './Cell';
 
 class Board extends Component {
 	render() {
---
+++ src/Cell.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Cell extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Cell...</h1>
+			</div>
+		);
+	}
+}
+
+Cell.propTypes = {};
+
+export default Cell;

9717e71782b451dea324a8bb1712cfa7bbaf7eb3
Barbara Szott <barbara.aleksy@gmail.com>
2019-10-03 15:03:11 +0200

    Create SapperIcon component to show different sapper icon at the top of the board (game/win/loose)

---
+++ src/SapperIcon.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Game.scss';
+
+class SapperIcon extends Component {
+	render() {
+		const { isGameFinished, didUserWin } = this.props;
+
+		const appearance = {
+			face  : 'sapper-face',
+			eyes  : 'sapper-eyes',
+			mouth : 'sapper-mouth'
+		};
+
+		if (isGameFinished) {
+			const modifier = didUserWin ? '_win' : '_loose';
+
+			for (let feature in appearance) {
+				appearance[feature] += modifier;
+			}
+		}
+
+		const { face, eyes, mouth } = appearance;
+
+		return (
+			<div className={face}>
+				<div className={eyes} />
+				<div className={mouth} />
+			</div>
+		);
+	}
+}
+
+SapperIcon.propTypes = {
+	isGameFinished : PropTypes.bool,
+	didUserWin     : PropTypes.bool
+};
+
+export default SapperIcon;

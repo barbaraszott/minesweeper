b2a3eae51022fa0081f01937bdadabb029a7fe46
Barbara Szott <barbara.aleksy@gmail.com>
2019-10-10 16:35:55 +0200

    Create separate CSS file for SapperIcon Component

--- src/SapperIcon.js
+++ src/SapperIcon.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './Game.scss';
+import './SapperIcon.scss';
 
 class SapperIcon extends Component {
 	render() {

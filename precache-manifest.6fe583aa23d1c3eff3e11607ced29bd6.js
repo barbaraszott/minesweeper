2a7289b869d0bf999772932519e3533e22226df7
Barbara Szott <barbara.aleksy@gmail.com>
2019-10-10 19:30:50 +0200

    Updates

---
+++ precache-manifest.6fe583aa23d1c3eff3e11607ced29bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620cefb87acf14e71d40a6ce5a56f61d",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "aedc38031a79ffb0dddf",
+    "url": "/minesweeper/static/css/main.47961f10.chunk.css"
+  },
+  {
+    "revision": "86f92bb6b302e3fcd3ad",
+    "url": "/minesweeper/static/js/2.4e37bcd4.chunk.js"
+  },
+  {
+    "revision": "aedc38031a79ffb0dddf",
+    "url": "/minesweeper/static/js/main.f251b495.chunk.js"
+  },
+  {
+    "revision": "ba9da0c9eb7e5b6048f9",
+    "url": "/minesweeper/static/js/runtime~main.59bc03dc.js"
+  },
+  {
+    "revision": "6f2e0ebe39b521575c17794098b6824a",
+    "url": "/minesweeper/static/media/pink-flag.6f2e0ebe.svg"
+  }
+]);

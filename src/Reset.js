f907bca8c5233761a0d4f66fc2a3b10c06287776
Barbara Szott <barbara.aleksy@gmail.com>
2019-10-10 18:16:56 +0200

    Create Reset Component to show button "play again" when the game is finished
    Styles - background-color and color depends if user won or loose

---
+++ src/Reset.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Reset.scss';
+
+class Reset extends Component {
+	render() {
+		const didUserWin = this.props.didUserWin;
+		return (
+			<div className={`reset ${didUserWin ? 'reset_win' : 'reset_loose'}`} onClick={this.props.onClick}>
+				Play again
+			</div>
+		);
+	}
+}
+
+Reset.propTypes = {};
+
+export default Reset;
